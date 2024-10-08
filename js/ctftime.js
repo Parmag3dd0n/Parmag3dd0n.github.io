82fd4122abd2a7ae862d9662d37e67ce66d7b33a
Francesco Berti <berti.francesco@proton.me>
2024-10-08 22:08:24 +0200

    Fixed bugs.

--- js/ctftime.js
+++ js/ctftime.js
@@ -7,15 +7,18 @@ fetch(apiUrl).then(response => {
 	const currentYear = new Date().getFullYear();
 	const rating = data.rating[currentYear];
 
-	const ratingPoints = rating.rating_points.toFixed(3);
-	const countryPlace = rating.country_place;
-	const ratingPlace = rating.rating_place;
-	
+	const ratingPoints = rating && rating.rating_points !== undefined
+		? rating.rating_points.toFixed(3): "temporarily unavailable";
+	const countryPlace = rating && rating.country_place !== undefined
+		? rating.country_place: "temporarily unavailable";
+	const ratingPlace = rating && rating.rating_place !== undefined
+		? rating.rating_place: "temporarily unavailable";
+
 	const text = "🏆 Rating points: " + ratingPoints + "<br>🇮🇹 Country place: " +
 		countryPlace + "<br>🌍 World place: " + ratingPlace;
-	document.getElementById('ratings').innerHTML = text;
+	document.getElementById("ratings").innerHTML = text;
 }).catch(err => {
-	const text = "🏆 Rating points: temporarily unavailable<br>🇮🇹 Country place: " +
-		"temporarily unavailable<br>🌍 World place: temporarily unavaliable";
-	document.getElementById('ratings').innerHTML = text;
+    const text = "🏆 Rating points: temporarily unavailable<br>🇮🇹 Country place: " +
+		"temporarily unavailable<br>🌍 World place: temporarily unavailable";
+    document.getElementById("ratings").innerHTML = text;
 });
